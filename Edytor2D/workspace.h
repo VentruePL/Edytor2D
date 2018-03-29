0f2525f5940b7be4846d417173f3f2170e6cfcdb
Mateusz Pieczaba <matrixgothic@gmail.com>
2018-03-29 12:58:41 +0200

    Test1

--- Edytor2D/workspace.h
+++ Edytor2D/workspace.h
@@ -35,6 +35,8 @@ namespace Edytor2D {
 			}
 		}
 	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Button^  button2;
 	protected:
 
 	private:
@@ -51,6 +53,8 @@ namespace Edytor2D {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -63,16 +67,39 @@ namespace Edytor2D {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &workspace::button1_Click);
 			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(82, 53);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"label1";
+			this->label1->Click += gcnew System::EventHandler(this, &workspace::label1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(12, 100);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &workspace::button2_Click);
+			// 
 			// workspace
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Name = L"workspace";
 			this->Text = L"Edytor 2D";
 			this->Load += gcnew System::EventHandler(this, &workspace::workspace_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -92,5 +119,10 @@ namespace Edytor2D {
 			this->Close();
 		}
 	}
-	};
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+		label1->Text = "Coœ tam";
+	}
+};
 }
