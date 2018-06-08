c484b0ba3860a19fbb4ea9be716ff64435ff0655
Tomasz Nowek <nowek1a@gmail.com>
2018-06-08 21:20:14 +0200

    wyswietlanie okna

---
+++ Edytor2D/Forma.h
@@ -0,0 +1,85 @@
+#pragma once
+
+namespace Edytor2D {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Podsumowanie informacji o Forma
+	/// </summary>
+	public ref class Forma : public System::Windows::Forms::Form
+	{
+	public:
+		Forma(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: W tym miejscu dodaj kod konstruktora
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Wyczyœæ wszystkie u¿ywane zasoby.
+		/// </summary>
+		~Forma()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^  label1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Wymagana zmienna projektanta.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+	protected:
+
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Wymagana metoda obs³ugi projektanta — nie nale¿y modyfikowaæ 
+		/// zawartoœæ tej metody z edytorem kodu.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(392, 123);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(71, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"testowe okno";
+			this->label1->Click += gcnew System::EventHandler(this, &Forma::label1_Click);
+			// 
+			// Forma
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(788, 420);
+			this->Controls->Add(this->label1);
+			this->Name = L"Forma";
+			this->Text = L"Forma";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	};
+}
