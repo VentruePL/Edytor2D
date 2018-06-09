465f7efe9096d9461f8a3c1296516b95b879a80a
Tomasz Nowek <nowek1a@gmail.com>
2018-06-09 02:59:17 +0200

    wyswietlanie

--- Edytor2D/Forma.h
+++ Edytor2D/Forma.h
@@ -21,8 +21,14 @@ namespace Edytor2D {
 			//
 			//TODO: W tym miejscu dodaj kod konstruktora
 			//
+			pictureBox1->Image = gcnew Bitmap(pictureBox1->Width, pictureBox1->Height);
+			g = Graphics::FromImage(pictureBox1->Image);
+		
 		}
-
+	
+	private:
+		Graphics ^ g;
+		
 	protected:
 		/// <summary>
 		/// Wyczyœæ wszystkie u¿ywane zasoby.
@@ -34,7 +40,10 @@ namespace Edytor2D {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Panel^  panel1;
+	protected:
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+
 	protected:
 
 	private:
@@ -44,8 +53,6 @@ namespace Edytor2D {
 		System::ComponentModel::Container ^components;
 	protected:
 
-
-
 #pragma region Windows Form Designer generated code
 		/// <summary>
 		/// Wymagana metoda obs³ugi projektanta — nie nale¿y modyfikowaæ 
@@ -53,33 +60,50 @@ namespace Edytor2D {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Forma::typeid));
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// label1
+			// panel1
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(392, 123);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(71, 13);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"testowe okno";
-			this->label1->Click += gcnew System::EventHandler(this, &Forma::label1_Click);
+			this->panel1->Controls->Add(this->pictureBox1);
+			this->panel1->Location = System::Drawing::Point(12, 12);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(764, 396);
+			this->panel1->TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.BackgroundImage")));
+			this->pictureBox1->Location = System::Drawing::Point(0, 3);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(761, 393);
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
 			// 
 			// Forma
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(788, 420);
-			this->Controls->Add(this->label1);
+			this->Controls->Add(this->panel1);
 			this->Name = L"Forma";
 			this->Text = L"Forma";
+			this->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Forma::Forma_Paint);
+			this->panel1->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
 	}
-	};
+	private: System::Void Forma_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
+		
+
+	}
+};
 }
