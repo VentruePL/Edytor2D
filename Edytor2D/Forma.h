3e13adb2365e1463b40fcc8c8fb15f8c67de8149
Tomasz Nowek <nowek1a@gmail.com>
2018-06-10 04:06:59 +0200

    dodano mozliwosc rysowania wybranych ksztaltow

--- Edytor2D/Forma.h
+++ Edytor2D/Forma.h
@@ -15,20 +15,32 @@ namespace Edytor2D {
 	public ref class Forma : public System::Windows::Forms::Form
 	{
 	public:
+
+		Point p1, p2;
+
+	public:
+
+	public:
 		Forma(void)
 		{
 			InitializeComponent();
 			//
 			//TODO: W tym miejscu dodaj kod konstruktora
 			//
+			list = gcnew ArrayList();
 		}
 
 	public:
 
 	private:
+
+		ArrayList ^ list;
 		Graphics ^ g;
 		Pen^ myPen;
 		Pen^ myPen2;
+		Pen^ myPen3;
+
+	private: System::Windows::Forms::ComboBox^  comboBox1;
 
 	protected:
 		/// <summary>
@@ -59,11 +71,17 @@ namespace Edytor2D {
 		/// Wymagana metoda obs³ugi projektanta — nie nale¿y modyfikowaæ 
 		/// zawartoœæ tej metody z edytorem kodu.
 		/// </summary>
+		ref struct Line
+		{
+			Point punkt1;
+			Point punkt2;
+		};
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Forma::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
 			this->panel1->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
@@ -71,9 +89,9 @@ namespace Edytor2D {
 			// panel1
 			// 
 			this->panel1->Controls->Add(this->pictureBox1);
-			this->panel1->Location = System::Drawing::Point(12, 12);
+			this->panel1->Location = System::Drawing::Point(12, 56);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(764, 396);
+			this->panel1->Size = System::Drawing::Size(764, 180);
 			this->panel1->TabIndex = 0;
 			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Forma::panel1_Paint);
 			// 
@@ -82,21 +100,34 @@ namespace Edytor2D {
 			this->pictureBox1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.BackgroundImage")));
 			this->pictureBox1->Location = System::Drawing::Point(0, 3);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(761, 393);
+			this->pictureBox1->Size = System::Drawing::Size(761, 174);
 			this->pictureBox1->TabIndex = 0;
 			this->pictureBox1->TabStop = false;
 			this->pictureBox1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Forma::pictureBox1_Paint);
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"linia", L"elipsa", L"prostokat" });
+			this->comboBox1->Location = System::Drawing::Point(651, 25);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(121, 21);
+			this->comboBox1->TabIndex = 1;
+			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &Forma::comboBox1_SelectedIndexChanged);
 			// 
 			// Forma
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(788, 420);
+			this->Controls->Add(this->comboBox1);
 			this->Controls->Add(this->panel1);
 			this->Name = L"Forma";
 			this->Text = L"Forma";
 			this->Load += gcnew System::EventHandler(this, &Forma::Forma_Load);
 			this->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Forma::Forma_Paint);
+			this->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &Forma::Forma_MouseDown);
+			this->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &Forma::Forma_MouseUp);
 			this->panel1->ResumeLayout(false);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
@@ -107,7 +138,15 @@ namespace Edytor2D {
 	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
 	}
 	private: System::Void Forma_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
+		
+		Graphics^ g = e->Graphics;
+		for (int i = 0; i < list->Count; i++)
 
+		{
+			Line^ pline = dynamic_cast<Line^>(list->default[i]);
+			g->DrawLine(myPen, pline->punkt1.X, pline->punkt1.Y, pline->punkt2.X, pline->punkt2.Y);
+
+		}
 	}
 	private: System::Void panel1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
 	}
@@ -120,10 +159,12 @@ namespace Edytor2D {
 	}
 	private: System::Void pictureBox1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
 		Graphics^ g = e->Graphics;
-		myPen = gcnew Pen(Color::Black, 4);
+		myPen = gcnew Pen(Color::Blue, 4);
 		myPen2 = gcnew Pen(Color::Red, 6);
-		g->DrawEllipse(myPen, 50, 100, 200, 200);
-		g->DrawRectangle(myPen2, 300, 60, 250, 250);
+		myPen3 = gcnew Pen(Color::Cyan, 6);
+		g->DrawEllipse(myPen, 50, 50, 100, 100);
+		g->DrawEllipse(myPen, 600, 50, 100, 100);
+		g->DrawRectangle(myPen2, 300, 15, 150, 150);
 	}
 	private: System::Void pictureBox1_MouseUp(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
 
@@ -131,5 +172,42 @@ namespace Edytor2D {
 	private: System::Void pictureBox1_MouseDown(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
 
 	}
-	};
+	private: System::Void comboBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		int selectedIndex = comboBox1->SelectedIndex;
+		Object^ selectedItem = comboBox1->SelectedItem;
+		MessageBox::Show("Wybrano: " + selectedItem->ToString() + "\n" + "Numer: " + selectedIndex.ToString());
+	}
+	
+private: System::Void Forma_MouseDown(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+	p1.X = e->X;
+	p1.Y = e->Y;
+}
+private: System::Void Forma_MouseUp(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+	p2.X = e->X;
+	p2.Y = e->Y;
+	Graphics^ g = CreateGraphics();
+	if (comboBox1->SelectedIndex == 0)
+	{
+		g->DrawLine(myPen, p1.X, p1.Y, p2.X, p2.Y);
+	}
+	else
+		if (comboBox1->SelectedIndex == 1)
+
+		{
+			g->DrawEllipse(myPen2, p1.X, p1.Y, 100, 100);
+		}
+		else
+			if (comboBox1->SelectedIndex == 2)
+
+			{
+				g->DrawRectangle(myPen3, p1.X, p1.Y, 100, 100);
+			}
+
+	Line^ pline = gcnew Line();
+	pline->punkt1 = p1;
+	pline->punkt2 = p2;
+	list->Add(pline);
+	delete g;
+}
+};
 }
