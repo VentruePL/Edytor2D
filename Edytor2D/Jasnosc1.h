36ff12e3d1cf6f7f395189fcdbe208438337e157
Bartosz Kaczmarczyk <bartoszk10@vp.pl>
2018-06-12 14:25:23 +0200

    Stworzenie suwaka do jasności

--- Edytor2D/Jasnosc1.h
+++ Edytor2D/Jasnosc1.h
@@ -20,6 +20,7 @@ namespace Edytor2D {
 			InitializeComponent();
 			image = gcnew Bitmap(Img);
 			pictureBox1->Image = gcnew Bitmap(image);
+			label1->Text = String::Format("{0:0.00}", value);
 			//
 			//TODO: W tym miejscu dodaj kod konstruktora
 			//
@@ -38,9 +39,12 @@ namespace Edytor2D {
 		}
 
 	private:
+		float value = 0;
 		Image ^ image;
 	private: System::Windows::Forms::Panel^  panel1;
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::TrackBar^  trackBar1;
+	private: System::Windows::Forms::Label^  label1;
 			 /// <summary>
 		/// Wymagana zmienna projektanta.
 		/// </summary>
@@ -55,8 +59,11 @@ namespace Edytor2D {
 		{
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->trackBar1 = (gcnew System::Windows::Forms::TrackBar());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->panel1->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// panel1
@@ -76,24 +83,52 @@ namespace Edytor2D {
 			this->pictureBox1->TabStop = false;
 			this->pictureBox1->Click += gcnew System::EventHandler(this, &Jasnosc::pictureBox1_Click);
 			// 
+			// trackBar1
+			// 
+			this->trackBar1->Location = System::Drawing::Point(1008, 28);
+			this->trackBar1->Maximum = 100;
+			this->trackBar1->Name = L"trackBar1";
+			this->trackBar1->Size = System::Drawing::Size(192, 45);
+			this->trackBar1->TabIndex = 1;
+			this->trackBar1->ValueChanged += gcnew System::EventHandler(this, &Jasnosc::OnChange);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label1->Location = System::Drawing::Point(1070, 58);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(70, 25);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"label1";
+			// 
 			// Jasnosc
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1039, 558);
+			this->ClientSize = System::Drawing::Size(1273, 558);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->trackBar1);
 			this->Controls->Add(this->panel1);
 			this->Name = L"Jasnosc";
 			this->Text = L"Jasnosc";
 			this->Load += gcnew System::EventHandler(this, &Jasnosc::Jasnosc_Load);
 			this->panel1->ResumeLayout(false);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void Jasnosc_Load(System::Object^  sender, System::EventArgs^  e) {
 	}
 	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void OnChange(System::Object^  sender, System::EventArgs^  e) {
+		value = trackBar1->Value;
+		label1 ->Text= String::Format("{0:0.00}", value);
 	}
 	};
 }
